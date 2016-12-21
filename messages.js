de1b4e191b78739a51c0123458c13164a161d670
Clément Sauvage <csauvage@users.noreply.github.com>
2016-12-21 13:32:01 +0100

    Update messages.js

--- messages.js
+++ messages.js
@@ -3,8 +3,8 @@ var messages = [{
 	"body": "Demander a Cyril d'acheter un video projecteur",
 	"link": "http://fnac.com"
 },{
-	"title": "Déployer le configurateur sur Github",
-	"body": "Au travail bande de branle-bites",
+	"title": "Déployer le configurateur sur Heroku",
+	"body": "Au travail bande de mange-bites",
 	"link": "http://github.com"
 },{
 	"title": "Aller travailler mon javacript",
