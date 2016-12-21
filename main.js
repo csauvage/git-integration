19bff893c1740cfe1c7637693328b189ab673478
Clément Sauvage <clement.sauvage@me.com>
2016-12-21 11:59:18 +0100

    Update package.jd

--- main.js
+++ main.js
@@ -5,20 +5,13 @@ let Express = require('express');
 let app = Express();
 
 let messages = require('./messages.js');
-let port = 3000;
+let port = 80;
 
 app.use(Express.static('./app'));
 
-//
-// app.get('/', function (req, res) {
-// 	res.send('Hello World!');
-// });
-
 app.get('/messages',(req,res)=>{
 	res.send(JSON.stringify(messages));
-})
-
-
+});
 
 app.listen(port,()=>{
 	console.log(`App listening on port ${port}`);
